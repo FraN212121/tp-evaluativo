82248b905acab46c7a1317d5129d07a485136b93
FraN212121 <franbpascua3@gmail.com>
2024-07-02 11:04:51 -0300

    empezando el carrusel y cards

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
+import { CardsComponent } from './components/cards/cards.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent,
+    CardsComponent,
+    CarruselComponent
+  ],
   imports: [
     CommonModule,
     InicioRoutingModule
+  ],
+  exports: [
+    InicioComponent,
+    CardsComponent
   ]
 })
 export class InicioModule { }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//COMPONENTES LOCALES Y GLOBALES
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+//COMPONENTES DEL MATERIAL
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
@@ -11,11 +15,17 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule
   ],
   exports:[
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule
   ]
 })
 export class SharedModule { }
